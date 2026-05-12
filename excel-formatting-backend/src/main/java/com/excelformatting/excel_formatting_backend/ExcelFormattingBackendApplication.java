1e1c2cb5e8acd70b1a3a53c0db9842eea946d29b
leshenn <leshenn0106@gmail.com>
2026-05-12 12:46:34 +0200

    initial commit

---
+++ excel-formatting-backend/src/main/java/com/excelformatting/excel_formatting_backend/ExcelFormattingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.excelformatting.excel_formatting_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelFormattingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelFormattingBackendApplication.class, args);
+	}
+
+}
---
+++ excel-formatting-backend/src/test/java/com/excelformatting/excel_formatting_backend/ExcelFormattingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelformatting.excel_formatting_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelFormattingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
